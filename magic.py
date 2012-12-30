c861f6432a5f811cdb62e2a2d174d39d3a8d8e31
Matt Grimes <mtgrimes@GRIMES-LAPTOP.(none)>
2012-12-30 17:43:35 -0600

    removed a dead branch

---
+++ magic.py
@@ -0,0 +1,6 @@
+class Spell:
+    def __init__(self,name,energy_type,modifier,effect):
+        self.name = name
+        self.energy_type = energy_type
+        self.modifier = modifier
+        self.effect = effect
---
+++ spells.py
@@ -0,0 +1,3 @@
+from magic import *
+spellFire = Spell('Fire','thermal',-1,'d')
+spellIce = Spell('Ice','thermal',1,'d')
